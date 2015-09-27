0368e81a986825a19650aa01bd27c8fd14a6310a
Omar Ibrahim <omar21cu@yahoo.es>
2015-09-27 13:15:52 +0100

    Adding Randomize script
    Fix Required: exclude Group tags from being randomized

---
+++ Unity MathLand/Assets/Scripts/RandomizePositions.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomizePositions : MonoBehaviour {
+
+	public GameObject numbers, operators;
+
+	// Use this for initialization
+	void Start () {
+		numbers = GameObject.FindGameObjectWithTag ("Number");
+		operators = GameObject.FindGameObjectWithTag ("Operator");
+
+		Transform[] childNumber = numbers.GetComponentsInChildren<Transform>();
+		Transform[] childOperator = operators.GetComponentsInChildren<Transform>();
+
+		foreach (Transform child in childNumber) {
+
+			Vector3 rndPosition = new Vector3(Random.Range(-15,15), 3, Random.Range(-15,15));
+
+			if (child.gameObject.tag.Equals("Number")){
+				child.gameObject.transform.position = rndPosition;
+			}
+		}
+
+		foreach (Transform child in childOperator) {
+
+			if (child.gameObject.tag.Equals("Operator")){
+				Vector3 rndPosition = new Vector3(Random.Range(-15,15), 3, Random.Range(-15,15));
+				child.gameObject.transform.position = rndPosition;
+			}
+		}
+	}
+}
