90dc9447f7aee409f99af13d8c14506ff53c0ff4
Omar Ibrahim <omar21cu@yahoo.es>
2015-09-27 14:37:56 +0100

    RandLight Fix

--- Unity MathLand/Assets/Scripts/RandomizePositions.cs
+++ Unity MathLand/Assets/Scripts/RandomizePositions.cs
@@ -14,20 +14,22 @@ public class RandomizePositions : MonoBehaviour {
 		Transform[] childOperator = operators.GetComponentsInChildren<Transform>();
 
 		foreach (Transform child in childNumber) {
+			float Yposition = child.gameObject.transform.position.y;
+			Vector3 rndPosition = new Vector3(Random.Range(-45,45), Yposition, Random.Range(-45,45));
 
-			Vector3 rndPosition = new Vector3(Random.Range(-15,15), 3, Random.Range(-15,15));
-
-			if (child.gameObject.tag.Equals("Number")){
+			if (!child.gameObject.tag.Equals("Number")){
 				child.gameObject.transform.position = rndPosition;
 			}
 		}
 
 		foreach (Transform child in childOperator) {
 
-			if (child.gameObject.tag.Equals("Operator")){
-				Vector3 rndPosition = new Vector3(Random.Range(-15,15), 3, Random.Range(-15,15));
+			if (!child.gameObject.tag.Equals("Operator")){
+				float Yposition = child.gameObject.transform.position.y;
+				Vector3 rndPosition = new Vector3(Random.Range(-45,45), Yposition, Random.Range(-45,45));
 				child.gameObject.transform.position = rndPosition;
 			}
 		}
 	}
+
 }
